dce82edef52a113803c078632426690359a9e48a
NewCastile <carlosaguanipa1@gmail.com>
2023-11-23 11:38:17 -0400

    removed providers from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
-import AuthProviders, { ChakraProviders, ReduxProvider } from "@/app/providers";
 import "./globals.css";
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
-        <AuthProviders>
-          <ChakraProviders>
-            <ReduxProvider>
-              <main>{children}</main>
-            </ReduxProvider>
-          </ChakraProviders>
-        </AuthProviders>
+        <main>{children}</main>
       </body>
     </html>
   );
