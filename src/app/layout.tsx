9dcd10b3efb4734d896ca93ab5137b5a4704fbbd
NewCastile <carlosaguanipa1@gmail.com>
2023-12-07 11:24:18 -0400

    removed async declaration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
